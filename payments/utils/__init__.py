08f59da4247fa786d2a725bd7cc24dee9790e2ae
David <dgx.arnold@gmail.com>
2024-05-03 16:47:48 +0200

    fix: add back compatibility

--- payments/utils/__init__.py
+++ payments/utils/__init__.py
@@ -7,3 +7,6 @@ from payments.utils.utils import (
 	erpnext_app_import_guard,
 	PAYMENT_SESSION_REF_KEY,
 )
+
+# compatibility with older erpnext versions <16
+from payments.utils.utils import get_payment_controller as get_payment_gateway_controller
