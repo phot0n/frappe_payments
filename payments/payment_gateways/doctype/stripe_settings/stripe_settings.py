6510e8f6f7251ff1e81c6a14182d85005874564f
Avunu LLC <mail@avu.nu>
2024-05-29 15:42:23 -0400

    refactor: relative stripe URL

--- payments/payment_gateways/doctype/stripe_settings/stripe_settings.py
+++ payments/payment_gateways/doctype/stripe_settings/stripe_settings.py
@@ -188,7 +188,7 @@ class StripeSettings(Document):
 				)
 
 	def get_payment_url(self, **kwargs):
-		return get_url(f"./stripe_checkout?{urlencode(kwargs)}")
+		return f"/stripe_checkout?{urlencode(kwargs)}"
 
 	def create_request(self, data):
 		import stripe
